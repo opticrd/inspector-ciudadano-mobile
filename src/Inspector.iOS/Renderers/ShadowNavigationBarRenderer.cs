d9334dedbe1d86138d5eca13956e79d5557be967
luismts <deivi_0326@hotmail.com>
2021-04-29 23:01:01 -0400

    Drop Shadows on Navigation Toolbar

---
+++ src/Inspector.iOS/Renderers/ShadowNavigationBarRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+using CoreGraphics;
+using Inspector.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(ShadowNavigationBarRenderer))]
+namespace Inspector.iOS.Renderers
+{
+	public class ShadowNavigationBarRenderer : NavigationRenderer
+	{
+		protected override void OnElementChanged(VisualElementChangedEventArgs e)
+		{
+			base.OnElementChanged(e);
+
+			if (this.Element == null) return;
+
+			NavigationBar.Layer.ShadowColor = UIColor.Gray.CGColor;
+			NavigationBar.Layer.ShadowOffset = new CGSize(0, 0);
+			NavigationBar.Layer.ShadowOpacity = 1;
+		}
+	}
+}
