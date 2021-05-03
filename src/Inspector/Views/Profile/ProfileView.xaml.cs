15f1eb4763d3987abd453053f7564df9f202b6f3
luismts <deivi_0326@hotmail.com>
2021-05-03 17:58:25 -0400

    New profile view added. This one is remplacing the profile page.

---
+++ src/Inspector/Views/Profile/ProfileView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Inspector.Views
+{
+    public partial class ProfileView : Grid
+    {
+        public ProfileView()
+        {
+            InitializeComponent();
+        }
+    }
+}
