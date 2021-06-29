2f089852e70da210e386d2e30d5a619e02189ab4
luismts <deivi_0326@hotmail.com>
2021-06-28 22:08:31 -0400

    Settings added

---
+++ src/Inspector/Framework/Utils/Keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inspector.Framework.Utils
+{
+    public static class NavigationKeys
+    {
+        public const string TicketAdded = nameof(TicketAdded);
+    }
+
+    public static class CacheKeys
+    {
+        public const string Tickets = nameof(Tickets);
+    }
+}
---
+++ src/Inspector/Framework/Utils/Settings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Xamarin.Essentials;
+
+namespace Inspector.Framework.Utils
+{
+    public static class Settings
+    {
+        /// <summary>
+        /// Get/Set if the user is logged in
+        /// </summary>
+        public static bool IsLoggedIn
+        {
+            get
+            {
+                var loggedInDate = Preferences.Get(IsLoggedInDateKey, IsLoggedInDateDefault);
+                var expirationDate = loggedInDate.AddHours(12);
+
+                if (expirationDate > DateTime.Now)
+                {
+                    return Preferences.Get(IsLoggedInKey, IsLoggedInDefault);
+                }
+
+                RemoveAllSettings();
+                return false;
+            }
+            set 
+            { 
+                Preferences.Set(IsLoggedInKey, value);
+                Preferences.Set(IsLoggedInDateKey, DateTime.Now);
+            }
+        }
+
+        private const string IsLoggedInKey = "isLoggedIn_key";
+        private static readonly bool IsLoggedInDefault = false;
+
+        private const string IsLoggedInDateKey = "isLoggedInDate_key";
+        private static readonly DateTime IsLoggedInDateDefault = new DateTime();
+
+        /// <summary>
+        /// Get/Set if the it is the first time in the app
+        /// </summary>
+        public static bool IsFirstTime
+        {
+            get => Preferences.Get(IsFirstTimeKey, IsFirstTimeDefault);
+            set => Preferences.Set(IsFirstTimeKey, value);
+        }
+
+        private const string IsFirstTimeKey = "isFirstTime_key";
+        private static readonly bool IsFirstTimeDefault = true;
+
+        /// <summary>
+        /// To remove the key from settings
+        /// </summary>
+        /// <param name="key"></param>
+        public static void RemoveSetting(string key) => Preferences.Remove(key);
+
+        /// <summary>
+        /// To remove all settings
+        /// </summary>
+        public static void RemoveAllSettings() => Preferences.Clear();
+    }
+}
