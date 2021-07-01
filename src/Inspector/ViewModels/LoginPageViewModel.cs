01a1aa96c61e1a6b3535e815a7505a30b0cebbae
luismts <deivi_0326@hotmail.com>
2021-07-01 00:46:57 -0400

    Validation rules added for the login process

--- src/Inspector/ViewModels/LoginPageViewModel.cs
+++ src/Inspector/ViewModels/LoginPageViewModel.cs
@@ -1,16 +1,41 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Inspector.Resources.Labels;
+using Plugin.ValidationRules;
+using Plugin.ValidationRules.Extensions;
+using Prism.Commands;
+using Prism.Navigation;
+using Prism.Services;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Inspector.ViewModels
 {
-    public class LoginPageViewModel : BindableBase
+    public class LoginPageViewModel : BaseViewModel
     {
-        public LoginPageViewModel()
+        public LoginPageViewModel(INavigationService navigationService, IPageDialogService dialogService) : base(navigationService, dialogService)
         {
+            Email = Validator.Build<string>()
+                .IsRequired(Message.FieldRequired)
+                .IsEmail(Message.InvalidEmail);
 
+            Password = Validator.Build<string>().IsRequired(Message.FieldRequired);
+
+            LoginCommand = new DelegateCommand(OnLoginCommandExecute);
+            ForgetPasswordCommand = new DelegateCommand(()=> dialogService.DisplayAlertAsync(General.ForgetPassword, "Contacte su supervisor para mas información.", "Ok"));
+        }
+
+        public Validatable<string> Password { get; set; }
+        public Validatable<string> Email { get; set; }
+
+        public ICommand LoginCommand { get; set; }
+        public ICommand ForgetPasswordCommand { get; set; }
+
+        async void OnLoginCommandExecute()
+        {
+            if (Email.Validate() & Email.Validate())
+            {
+                //TODO: Call the server here
+                await _navigationService.NavigateAsync("NavigationPage/HomePage");
+            }
         }
     }
 }
