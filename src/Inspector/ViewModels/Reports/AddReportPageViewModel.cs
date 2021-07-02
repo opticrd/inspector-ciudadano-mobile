549941b592710ca260d9206d988e2b7e144b4f76
luismts <deivi_0326@hotmail.com>
2021-07-02 00:33:08 -0400

    Validations added

--- src/Inspector/ViewModels/Reports/AddReportPageViewModel.cs
+++ src/Inspector/ViewModels/Reports/AddReportPageViewModel.cs
@@ -1,6 +1,13 @@
 ﻿
+using Inspector.Resources.Labels;
+using Plugin.ValidationRules;
+using Plugin.ValidationRules.Extensions;
+using Prism.Commands;
 using Prism.Navigation;
 using Prism.Services;
+using System;
+using System.Windows.Input;
+using UIModule.Helpers.Rules;
 
 namespace Inspector.ViewModels
 {
@@ -8,7 +15,35 @@ namespace Inspector.ViewModels
     {
         public AddReportPageViewModel(INavigationService navigationService, IPageDialogService dialogService) : base(navigationService, dialogService)
         {
+            ID = Validator.Build<string>()
+                .IsRequired(Message.FieldRequired)
+                .WithRule(new CedulaRule());
 
+            Address = Validator.Build<string>().IsRequired(Message.FieldRequired);
+
+            ReportCommand = new DelegateCommand(ReportCommandExecute);
         }
+
+       
+        public Validatable<string> ID { get; set; }
+        public Validatable<string> Address { get; set; }
+
+        public int StateSelected { get; set; }
+        public int IncidentSelected { get; set; }
+        public int CategorySelected { get; set; }
+        public int xSelected { get; set; }
+        public string CitizenName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int InstitutionSelected { get; set; }
+        public string Comments { get; set; }
+
+
+        public ICommand ReportCommand { get; set; }
+
+        private void ReportCommandExecute()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
