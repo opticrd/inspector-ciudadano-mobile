906f8bdb1c9b17dba484276dfb8d1cad8a79ed5b
luismts <deivi_0326@hotmail.com>
2021-07-05 13:35:54 -0400

    Keys updated

--- src/Inspector/Framework/Utils/Keys.cs
+++ src/Inspector/Framework/Utils/Keys.cs
@@ -7,10 +7,17 @@ namespace Inspector.Framework.Utils
     public static class NavigationKeys
     {
         public const string TicketAdded = nameof(TicketAdded);
+        public const string HomePage = "NavigationPage/HomePage";
     }
 
     public static class CacheKeys
     {
         public const string Tickets = nameof(Tickets);
+        public const string ZammadAccount = nameof(ZammadAccount);
+    }
+
+    public static class AppKeys
+    {
+        public static string ZammadApiBaseUrl = AppSettingsManager.Settings["ZammadApiBaseUrl"];
     }
 }
