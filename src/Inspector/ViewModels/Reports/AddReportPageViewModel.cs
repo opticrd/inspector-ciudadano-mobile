5fce60f170420c268a29cbac1ade9fa748135497
luismts <deivi_0326@hotmail.com>
2021-07-10 00:30:43 -0400

    Base logic for ticket form

--- src/Inspector/ViewModels/Reports/AddReportPageViewModel.cs
+++ src/Inspector/ViewModels/Reports/AddReportPageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿
 using Inspector.Framework.Services;
+using Inspector.Framework.Utils;
+using Inspector.Models;
 using Inspector.Resources.Labels;
 using Plugin.ValidationRules;
 using Plugin.ValidationRules.Extensions;
@@ -7,13 +9,20 @@ using Prism.Commands;
 using Prism.Navigation;
 using Prism.Services;
 using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using UIModule.Helpers.Rules;
+using Zammad.Client;
+using Zammad.Client.Resources;
 
 namespace Inspector.ViewModels
 {
     public class AddReportPageViewModel : BaseViewModel
     {
+        TicketClient _ticketClient;
+        User _userAccount;
+
         public AddReportPageViewModel(INavigationService navigationService, IPageDialogService dialogService, ICacheService cacheService) : base(navigationService, dialogService, cacheService)
         {
             ID = Validator.Build<string>()
@@ -22,13 +31,14 @@ namespace Inspector.ViewModels
 
             Address = Validator.Build<string>().IsRequired(Message.FieldRequired);
 
+            States = new ObservableCollection<StateTicket>(StateTicket.GetStatesForNewTicket());
+
             ReportCommand = new DelegateCommand(ReportCommandExecute);
         }
 
-       
         public Validatable<string> ID { get; set; }
         public Validatable<string> Address { get; set; }
-
+        public ObservableCollection<StateTicket> States { get; set; }
         public int StateSelected { get; set; }
         public int IncidentSelected { get; set; }
         public int CategorySelected { get; set; }
@@ -38,12 +48,39 @@ namespace Inspector.ViewModels
         public int InstitutionSelected { get; set; }
         public string Comments { get; set; }
 
-
         public ICommand ReportCommand { get; set; }
 
-        private void ReportCommandExecute()
+        private async void Init()
         {
-            throw new NotImplementedException();
+            var account = await _cacheService.GetSecureObject<ZammadAccount>(CacheKeys.ZammadAccount);
+            _ticketClient = account.CreateTicketClient();
+
+            _userAccount = await _cacheService.GetSecureObject<User>(CacheKeys.UserAccount);
+        }
+
+        private async void ReportCommandExecute()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            var ticket = await _ticketClient.CreateTicketAsync(
+                new Ticket
+                {
+                    Title = "Help me!",
+                    GroupId = 1,
+                    CustomerId = 1,
+                    OwnerId = 1,
+                },
+                new TicketArticle
+                {
+                    Subject = "Help me!!!",
+                    Body = "Nothing Work!",
+                    Type = "note",
+                });
+
+            IsBusy = false;
         }
 
     }
