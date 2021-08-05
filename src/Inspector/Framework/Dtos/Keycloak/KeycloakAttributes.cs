08fb6460f0b50e0890995aa3d1ba1ed6a0a8904d
Enmanuel Toribio <enmanuel@torib.io>
2021-08-04 20:38:41 -0400

    Adding keycloak client

---
+++ src/Inspector/Framework/Dtos/Keycloak/KeycloakAttributes.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inspector.Framework.Dtos.Keycloak
+{
+    public class KeycloakAttributes
+    {
+        [JsonProperty("cedula")]
+        public List<string> Cedula { get; set; }
+    }
+}
---
+++ src/Inspector/Framework/Interfaces/IKeycloakApi.cs
@@ -0,0 +1,19 @@
+﻿using Inspector.Framework.Dtos;
+using Inspector.Framework.Dtos.Keycloak;
+using Inspector.Framework.Dtos.Keycloak.Keycloak;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inspector.Framework.Interfaces
+{
+    public interface IKeycloakApi
+    {
+        [Post("/auth/realms/master/protocol/openid-connect/token")]
+        OAuthToken Authenticate([Body(BodySerializationMethod.Serialized)] TokenRequestBody tokenRequestBody);
+
+        [Get("/auth/admin/realms/master/users")]
+        KeycloakUser GetUser([Header("Authorization")]string token, string search);
+    }
+}
