cb6e19475a616e704dd6d72a3c78621088baa0ec
Enmanuel Toribio <enmanuel@torib.io>
2021-08-10 10:02:04 -0400

    Bumping version script updated

--- src/Inspector.Android/appcenter-pre-build.sh
+++ src/Inspector.Android/appcenter-pre-build.sh
@@ -2,6 +2,7 @@
 
 SPLASH_ACTIVITY="$BUILD_REPOSITORY_LOCALPATH/src/Inspector.Android/SplashActivity.cs"
 MANIFEST="$BUILD_REPOSITORY_LOCALPATH/src/Inspector.Android/Properties/AndroidManifest.xml"
+MANIFEST_AGENT ="$BUILD_REPOSITORY_LOCALPATH/src/Inspector.Android/Properties/AndroidManifestAgent.xml"
 APPSETTINGS="$BUILD_REPOSITORY_LOCALPATH/src/Inspector/appsettings.json"
 VERSIONNAME=`grep versionName ${MANIFEST} | sed 's/.*versionName\s*=\s*\"\([^\"]*\)\".*/\1/g'`
 
@@ -16,6 +17,21 @@ rm -f ${MANIFEST}.bak
 # Print out file for reference
 cat $MANIFEST
 echo "Updated manifest!"
+
+sed -i.bak "s/android:versionName="\"${VERSIONNAME}\""/android:versionName="\"1.0.${APPCENTER_BUILD_ID}\""/" ${MANIFEST_AGENT}
+rm -f ${MANIFEST_AGENT}.bak
+sed -i.bak "s/android:versionCode="\"1\""/android:versionCode="\"${APPCENTER_BUILD_ID}\""/" ${MANIFEST_AGENT}
+rm -f ${MANIFEST_AGENT}.bak
+
+sed -i.bak "s/package="\"do.gob.ogtic.reportero\""/package="\"${APP_BUNDLE_ID}\""/" ${MANIFEST_AGENT}
+rm -f ${MANIFEST_AGENT}.bak
+
+
+# Print out file for reference
+cat $MANIFEST_AGENT
+echo "Updated manifest!"
+
+
 
 sed -i.bak "s/Label="\"Inspector\""/Label="\"${APP_NAME}\""/" ${SPLASH_ACTIVITY}
 rm -f ${SPLASH_ACTIVITY}.bak
