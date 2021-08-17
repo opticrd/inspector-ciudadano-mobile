5d8de4aa5c26c7ff00944dd30338d8e3b46fad69
luismts <deivi_0326@hotmail.com>
2021-08-17 13:06:19 -0400

    Support for Section, Neighhborhood and SubNeighhborhood added.

--- src/Inspector/Framework/Dtos/Response.cs
+++ src/Inspector/Framework/Dtos/Response.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using Inspector.Framework.Converters;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Inspector.Framework.Dtos
 {
@@ -12,5 +14,15 @@ namespace Inspector.Framework.Dtos
 
         [JsonProperty("payload")]
         public T Payload { get; set; }
+    }
+
+    public class ResponseZone
+    {
+        [JsonProperty("valid")]
+        public bool Valid { get; set; }
+
+        [JsonProperty("data")]
+        [JsonConverter(typeof(SingleOrArrayConverter<Zone>))]
+        public List<Zone> Data { get; set; }
     }
 }
