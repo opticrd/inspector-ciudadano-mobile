839d8b397310720a92ed1ecd5f8d64ba0945b63a
luismts <deivi_0326@hotmail.com>
2021-09-11 02:58:56 -0400

    Picker custom renderers added in order to remove the underline

---
+++ src/Inspector.Android/Renderers/CustomPickerRenderer.cs
@@ -0,0 +1,29 @@
+﻿
+using Android.Content;
+using Android.Graphics.Drawables;
+using Inspector.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(CustomPickerRenderer))]
+namespace Inspector.Droid.Renderers
+{
+    class CustomPickerRenderer : PickerRenderer
+    {
+        public CustomPickerRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                GradientDrawable gd = new GradientDrawable();
+                gd.SetStroke(0, Android.Graphics.Color.Transparent);
+                Control.SetBackground(gd);
+            }
+        }
+    }
+}
---
+++ src/Inspector.iOS/Renderers/CustomPickerRenderer.cs
@@ -0,0 +1,23 @@
+﻿
+using Inspector.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(CustomPickerRenderer))]
+namespace Inspector.iOS.Renderers
+{
+    class CustomPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            //var view = e.NewElement as Picker;
+
+            if (Control != null)
+            {
+                Control.BorderStyle = UITextBorderStyle.None;
+            }            
+        }
+    }
+}
