ab81ea986d538758a7a2c1ed28c483fe1855c450
luismts <deivi_0326@hotmail.com>
2021-09-14 01:23:40 -0400

    CustomNavigationPage added. But not in used yet.

---
+++ src/Inspector/Framework/Controls/CustomNavigationPage.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace Inspector.Framework.Controls
+{
+    public class CustomNavigationPage : Xamarin.Forms.NavigationPage
+    {
+        public CustomNavigationPage()
+        {
+            On<iOS>().SetHideNavigationBarSeparator(true);
+            //var x = On<iOS>().HideNavigationBarSeparator();
+        }
+    }
+}
